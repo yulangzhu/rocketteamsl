102481027740b8c949ac11b41bc84c4be074a34f
yulangzhu <121367624+yulangzhu@users.noreply.github.com>
2023-02-13 04:54:18 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,12 @@
 <?php
-    header('Location: https://htmlpreview.github.io/?https://raw.githubusercontent.com/yulangzhu/rocketteamsl/main/index.html');
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        // Aquí podries comprovar les credencials si fos necessari
+        // ...
+
+        // Mostrar un missatge d'èxit
+        echo "Sessió iniciada correctament!";
+
+        // Redirigir a una altra pàgina
+        header("Refresh: 2; url=https://htmlpreview.github.io/?https://raw.githubusercontent.com/yulangzhu/rocketteamsl/main/index.html");
+    }
 ?>
